7e39efbdb054cc75b7f17e01ee378f5c34e17045
Caroline <carolinelappleby@gmail.com>
2017-10-18 17:15:48 +0100

    Rename and Basic Functions
    Gave html file correct name and added basic calculator functions.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Javascript Calculator
+# Javascript Calculator
 
 This is my repository for my calculator!
 Work in progress.
---
+++ calculator.js
@@ -0,0 +1,7 @@
+
+
+function addInput(button) {
+  document.innerHTML.getElementById("display") = button.value;
+}
+
+var sum = [];
